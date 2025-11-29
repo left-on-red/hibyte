42e084a2f53f3cd114af004664a57757852bc24e
red <tachatz214@gmail.com>
2025-11-28 21:13:58 -0600

    initial commit

---
+++ src/reader.ts
@@ -0,0 +1,225 @@
+import { FileHandle, open } from "fs/promises";
+
+export interface IHiReader {
+	string(length: number, encoding: BufferEncoding, offset: number): string | Promise<string>;
+	int(length: number, offset: number): number | Promise<number>;
+	uint(length: number, offset: number): number | Promise<number>;
+	int8(length: number): number | Promise<number>;
+	uint8(length: number): number | Promise<number>;
+	int16(length: number): number | Promise<number>;
+	uint16(length: number): number | Promise<number>;
+	int32(length: number): number | Promise<number>;
+	uint32(length: number): number | Promise<number>;
+	int64(length: number): bigint | Promise<bigint>;
+	uint64(length: number): bigint | Promise<bigint>;
+	float(offset: number): number | Promise<number>;
+	double(offset: number): number | Promise<number>;
+	bytes(length: number, offset: number): Buffer | Promise<Buffer>;
+
+	seek(offset: number): void;
+	setPosition(position: number): void;
+	getPosition(): number;
+}
+
+class HiFileReader implements IHiReader {
+	private handle: FileHandle;
+	private endian: 'BE' | 'LE';
+	private position: number;
+
+	constructor(handle: FileHandle, endian?: 'BE' | 'LE') {
+		this.handle = handle;
+		this.endian = endian ?? 'BE';
+		this.position = 0;
+	}
+
+	async string(length: number, encoding: BufferEncoding = 'utf8', offset = this.position) {
+		const buffer = await this.bytes(length, offset);
+		const result = buffer.toString(encoding, 0x00, length);
+		return result;
+	}
+
+	async int(length: number, offset: number = this.position) {
+		const buffer = await this.bytes(length, offset);
+		const result = this.endian === 'BE' ? buffer.readIntBE(0x00, length) : buffer.readIntLE(0x00, length);
+		return result;
+	}
+
+	async uint(length: number, offset: number = this.position) {
+		const buffer = await this.bytes(length, offset);
+		const result = this.endian === 'BE' ? buffer.readUIntBE(0x00, length) : buffer.readUIntLE(0x00, length);
+		return result;
+	}
+
+	async int8(offset: number = this.position) { return await this.int(1, offset); }
+
+	async uint8(offset: number = this.position) { return await this.uint(1, offset); }
+
+	async int16(offset: number = this.position) { return await this.int(2, offset); }
+
+	async uint16(offset: number = this.position) { return await this.uint(2, offset); }
+
+	async int32(offset: number = this.position) { return await this.int(4, offset); }
+
+	async uint32(offset: number = this.position) { return await this.uint(4, offset); }
+
+	async int64(offset: number = this.position) {
+		const buffer = await this.bytes(8, offset);
+		const result = this.endian === 'BE' ? buffer.readBigInt64BE(0x00) : buffer.readBigInt64LE(0x00);
+		return result;
+	}
+
+	async uint64(offset: number = this.position) {
+		const buffer = await this.bytes(8, offset);
+		const result = this.endian === 'BE' ? buffer.readBigUInt64BE(0x00) : buffer.readBigUInt64LE(0x00);
+		return result;
+	}
+
+	async float(offset: number = this.position) {
+		const buffer = await this.bytes(4, offset);
+		const result = this.endian === 'BE' ? buffer.readFloatBE(0x00) : buffer.readFloatLE(0x00);
+		return result;
+	}
+
+	async double(offset: number = this.position) {
+		const buffer = await this.bytes(8, offset);
+		const result = this.endian === 'BE' ? buffer.readDoubleBE(0x00) : buffer.readDoubleLE(0x00);
+		return result;
+	}
+
+	async bytes(length: number, offset = this.position) {
+		const buffer = Buffer.alloc(length);
+		const { bytesRead } = await this.handle.read({ offset: 0x00, position: offset, length: length, buffer });
+		if (length !== bytesRead) {
+			throw new Error(`attempted an out-of-bounds read (start=0x${this.position.toString(16).toUpperCase()}, length=${length})`);
+		} else {
+			this.position = offset + length;
+			return buffer;
+		}
+	}
+
+	seek(offset: number) {
+		this.position += offset;
+		if (this.position < 0) {
+			this.position = 0;
+		}
+	}
+
+	setPosition(offset: number) {
+		this.position = offset;
+	}
+
+	getPosition() {
+		return this.position;
+	}
+}
+
+class HiBufferReader implements IHiReader {
+	buffer: Buffer;
+	endian: 'BE' | 'LE';
+	private position: number;
+
+	constructor(buffer: Buffer, endian?: 'BE' | 'LE') {
+		this.buffer = buffer;
+		this.endian = endian ?? 'BE';
+		this.position = 0;
+	}
+
+	string(length: number, encoding: BufferEncoding = 'utf8', offset: number = this.position,) {
+		const result = this.buffer.toString(encoding, offset, this.position + length)
+		this.position = offset + length;
+		return result;
+	}
+
+	int(length: number, offset: number = this.position) {
+		const result = this.endian === 'BE' ? this.buffer.readIntBE(offset, length) : this.buffer.readIntLE(offset, length);
+		this.position = offset + length;
+		return result;
+	}
+
+	uint(length: number, offset: number = this.position) {
+		const result = this.endian === 'BE' ? this.buffer.readUIntBE(offset, length) : this.buffer.readUIntLE(offset, length);
+		this.position = offset + length;
+		return result;
+	}
+
+	int8(offset: number = this.position) { return this.int(1, offset); }
+
+	uint8(offset: number = this.position) { return this.uint(1, offset); }
+
+	int16(offset: number = this.position) { return this.int(2, offset); }
+
+	uint16(offset: number = this.position) { return this.uint(2, offset); }
+
+	int32(offset: number = this.position) { return this.int(4, offset); }
+
+	uint32(offset: number = this.position) { return this.uint(4, offset); }
+
+	int64(offset: number = this.position) {
+		const result = this.endian === 'BE' ? this.buffer.readBigInt64BE(offset) : this.buffer.readBigInt64LE(offset);
+		this.position = offset + 8;
+		return result;
+	}
+
+	uint64(offset: number = this.position) {
+		const result = this.endian === 'BE' ? this.buffer.readBigUInt64BE(offset) : this.buffer.readBigUInt64LE(offset);
+		this.position = offset + 8;
+		return result;
+	}
+
+	float(offset: number = this.position) {
+		const result = this.endian === 'BE' ? this.buffer.readFloatBE(offset) : this.buffer.readFloatLE(offset);
+		this.position = offset + 4;
+		return result;
+	}
+
+	double(offset: number = this.position) {
+		const result = this.endian === 'BE' ? this.buffer.readDoubleBE(offset) : this.buffer.readDoubleBE(offset);
+		this.position = offset + 8;
+		return result;
+	}
+
+	bytes(length: number, offset: number = this.position) {
+		const result = this.buffer.subarray(offset, offset + length);
+		this.position = offset + length;
+		return result;
+	}
+
+	push(buffer: Buffer) {
+		this.buffer = Buffer.concat([this.buffer, buffer]);
+	}
+
+	seek(offset: number) {
+		this.position += offset;
+		if (this.position < 0) {
+			this.position = 0;
+		} else if (this.position > this.buffer.length - 1) {
+			this.position = this.buffer.length - 1;
+		}
+	}
+
+	setPosition(position: number) {
+		this.position = position;
+		if (this.position < 0) {
+			this.position = 0;
+		} else if (this.position > this.buffer.length - 1) {
+			this.position = this.buffer.length - 1;
+		}
+	}
+
+	getPosition() {
+		return this.position;
+	}
+}
+
+export function createReader(filePath: string, endian?: 'BE' | 'LE'): Promise<HiFileReader>;
+export function createReader(handle: FileHandle, endian?: 'BE' | 'LE'): HiFileReader;
+export function createReader(buffer: Buffer, endian?: 'BE' | 'LE'): HiBufferReader;
+export function createReader(arg: string | FileHandle | Buffer, endian: 'BE' | 'LE' = 'BE') {
+		if (typeof arg === 'string') {
+			return open(arg).then(x => new HiFileReader(x, endian));
+		} else if (Buffer.isBuffer(arg)) {
+			return new HiBufferReader(arg, endian);
+		} else {
+			return new HiFileReader(arg, endian);
+		}
+}
---
+++ src/writer.ts
@@ -0,0 +1,305 @@
+import { FileHandle, open } from "fs/promises";
+
+export interface IHiWriter {
+	string(value: string, encoding?: BufferEncoding, offset?: number): number | Promise<number>;
+	int(value: number, size: number, offset?: number): number | Promise<number>;
+	uint(value: number, size: number, offset?: number): number | Promise<number>;
+	int8(value: number, offset?: number): number | Promise<number>;
+	uint8(value: number, offset?: number): number | Promise<number>;
+	int16(value: number, offset?: number): number | Promise<number>;
+	uint16(value: number, offset?: number): number | Promise<number>;
+	int32(value: number, offset?: number): number | Promise<number>;
+	uint32(value: number, offset?: number): number | Promise<number>;
+	int64(value: bigint, offset?: number): number | Promise<number>;
+	uint64(value: bigint, offset?: number): number | Promise<number>;
+	float(value: number, offset?: number): number | Promise<number>;
+	double(value: number, offset?: number): number | Promise<number>;
+	bytes(value: Buffer, offset?: number): number | Promise<number>;
+
+	seek(offset: number): void;
+	setPosition(position: number): void;
+	getPosition(): number;
+}
+
+function validateInt(value: number, size: number) {
+	if (Math.floor(value) !== value) {
+		throw new Error(`invalid integer value: ${value}`);
+	} else if (size < 1 || size > 6 || Math.floor(size) !== size) {
+		throw new Error(`invalid size: ${size} (expected: 1-6)`);
+	} else if (value > 2 ** (size * 8) / 2 - 1 || value < -(2 ** (size * 8) / 2 - 1)) {
+		throw new Error(`invalid ${size * 8}-bit integer value: ${value}`);
+	}
+}
+
+function validateUInt(value: number, size: number) {
+	if (value < 0 || Math.floor(value) !== value) {
+		throw new Error(`invalid unsigned-integer value: ${value}`);
+	} else if (size < 1 || size > 6 || Math.floor(size) !== size) {
+		throw new Error(`invalid size: ${size} (expected: 1-6)`);
+	} else if (value > 2 ** (size * 8) - 1) {
+		throw new Error(`invalid ${size * 8}-bit unsigned-integer value: ${value}`);
+	}
+}
+
+class HiFileWriter implements IHiWriter {
+	handle: FileHandle;
+	endian: 'BE' | 'LE';
+	private position: number;
+
+	constructor(handle: FileHandle, endian?: 'BE' | 'LE') {
+		this.handle = handle;
+		this.endian = endian ?? 'BE';
+		this.position = 0;
+	}
+
+	async string(value: string, encoding: BufferEncoding = 'utf8', offset = this.position) {
+		const { bytesWritten } = await this.handle.write(value, offset, encoding);
+		this.position = offset + bytesWritten;
+		return bytesWritten;
+	}
+
+	async int(value: number, size: number, offset: number = this.position) {
+		validateInt(value, size);
+
+		const buffer = Buffer.alloc(size);
+		if (this.endian === 'BE') {
+			buffer.writeIntBE(value, 0x00, size);
+		} else {
+			buffer.writeIntLE(value, 0x00, size);
+		}
+
+		return await this.bytes(buffer, offset);
+	}
+
+	async uint(value: number, size: number, offset: number = this.position) {
+		validateUInt(value, size);
+
+		const buffer = Buffer.alloc(size);
+		if (this.endian === 'BE') {
+			buffer.writeUIntBE(value, 0x00, size);
+		} else {
+			buffer.writeUIntLE(value, 0x00, size);
+		}
+
+		return await this.bytes(buffer, offset);
+	}
+
+	async int8(value: number, offset: number = this.position) { return await this.int(value, 1, offset); }
+
+	async uint8(value: number, offset: number = this.position) { return await this.uint(value, 1, offset); }
+
+	async int16(value: number, offset: number = this.position) { return await this.int(value, 2, offset); }
+
+	async uint16(value: number, offset: number = this.position) { return await this.uint(value, 2, offset); }
+
+	async int32(value: number, offset: number = this.position) { return await this.int(value, 4, offset); }
+
+	async uint32(value: number, offset: number = this.position) { return await this.uint(value, 4, offset); }
+
+	async int64(value: bigint, offset: number = this.position) {
+		if (value < 0) {
+			throw new Error(`invalid 64-bit integer value: ${value}`);
+		}
+
+		const buffer = Buffer.alloc(8);
+		if (this.endian === 'BE') {
+			buffer.writeBigInt64BE(value, 0x00);
+		} else {
+			buffer.writeBigInt64LE(value, 0x00);
+		}
+
+		return await this.bytes(buffer, offset);
+	}
+
+	async uint64(value: bigint, offset: number = this.position) {
+		const buffer = Buffer.alloc(8);
+		if (this.endian === 'BE') {
+			buffer.writeBigUint64BE(value, 0x00);
+		} else {
+			buffer.writeBigUint64LE(value, 0x00);
+		}
+
+		return await this.bytes(buffer, offset);
+	}
+
+	async float(value: number, offset: number = this.position) {
+		// handle invalid value?
+		const buffer = Buffer.alloc(4);
+		if (this.endian === 'BE') {
+			buffer.writeFloatBE(value, 0x00);
+		} else {
+			buffer.writeFloatLE(value, 0x00);
+		}
+
+		return await this.bytes(buffer, offset);
+	}
+
+	async double(value: number, offset: number = this.position) {
+		const buffer = Buffer.alloc(8);
+		if (this.endian === 'BE') {
+			buffer.writeDoubleBE(value, 0x00);
+		} else {
+			buffer.writeDoubleLE(value, 0x00);
+		}
+
+		return await this.bytes(buffer, offset);
+	}
+
+	async bytes(value: Buffer, offset = this.position) {
+		const { bytesWritten } = await this.handle.write(value, offset);
+		this.position = offset + bytesWritten;
+		return bytesWritten;
+	}
+
+	seek(offset: number) {
+		this.position += offset;
+		if (this.position < 0) {
+			this.position = 0;
+		}
+	}
+
+	setPosition(offset: number) {
+		this.position = offset;
+	}
+
+	getPosition() {
+		return this.position;
+	}
+}
+
+class HiBufferWriter implements IHiWriter {
+	private buffer: Buffer;
+	private endian: 'BE' | 'LE';
+	private chunkSize: number;
+	private position: number = 0;
+	private size: number = 0;
+
+	constructor(buffer?: Buffer, endian?: 'BE' | 'LE', chunkSize?: number) {
+		this.endian = endian ?? 'BE';
+		this.chunkSize = chunkSize ?? 1024;
+		if (buffer) {
+			this.buffer = buffer;
+			this.position = this.size = this.buffer.length;
+		} else {
+			this.buffer = Buffer.alloc(1024);
+		}
+	}
+
+	private grow(size: number) {
+		// TODO: test
+		while (this.position + size > this.buffer.length) {
+			this.buffer = Buffer.concat([this.buffer, Buffer.alloc(this.chunkSize)]);
+		}
+
+		if (this.position + size > this.size) {
+			this.size = this.position + size;
+		}
+	}
+
+
+
+	string(value: string, encoding: BufferEncoding = 'utf8', offset: number = this.position) {
+		const buffer = Buffer.from(value, encoding);
+		this.grow(buffer.length);
+		this.buffer.fill(buffer, offset, offset + buffer.length);
+		this.position = offset + buffer.length;
+		return buffer.length;
+	}
+
+	int(value: number, size: number, offset: number = this.position) {
+		validateInt(value, size);
+		this.grow(size);
+		this.position = this.endian === 'BE' ? this.buffer.writeIntBE(value, offset, size) : this.buffer.writeIntLE(value, offset, size);
+		return size;
+	}
+
+	uint(value: number, size: number, offset: number = this.position) {
+		validateUInt(value, size);
+		this.grow(size);
+		this.position = this.endian === 'BE' ? this.buffer.writeUIntBE(value, offset, size) : this.buffer.writeUIntLE(value, offset, size);
+		return size;
+	}
+
+	int8(value: number, offset: number = this.position) { return this.int(value, 1, offset); }
+
+	uint8(value: number, offset: number = this.position) { return this.uint(value, 1, offset); }
+
+	int16(value: number, offset: number = this.position) { return this.int(value, 2, offset); }
+
+	uint16(value: number, offset: number = this.position) { return this.uint(value, 2, offset); }
+
+	int32(value: number, offset: number = this.position) { return this.int(value, 4, offset); }
+
+	uint32(value: number, offset: number = this.position) { return this.uint(value, 4, offset); }
+
+	int64(value: bigint, offset: number = this.position) {
+		this.grow(8);
+		this.position = this.endian === 'BE' ? this.buffer.writeBigInt64BE(value, offset) : this.buffer.writeBigInt64LE(value, offset);
+		return 8;
+	}
+
+	uint64(value: bigint, offset: number = this.position) {
+		this.grow(8);
+		this.position = this.endian === 'BE' ? this.buffer.writeBigUInt64BE(value, offset) : this.buffer.writeBigUInt64LE(value, offset);
+		return 8;
+	}
+
+	float(value: number, offset: number = this.position) {
+		this.grow(4);
+		this.position = this.endian === 'BE' ? this.buffer.writeFloatBE(value, offset) : this.buffer.writeFloatLE(value, offset);
+		return 4;
+	}
+
+	double(value: number, offset: number = this.position) {
+		this.grow(8);
+		this.position = this.endian === 'BE' ? this.buffer.writeDoubleBE(value, offset) : this.buffer.writeDoubleLE(value, offset);
+		return 8;
+	}
+
+	bytes(value: Buffer, offset: number = this.position) {
+		// TODO: test
+		this.grow(value.length);
+		this.buffer.fill(value, offset, offset + value.length);
+		this.position = offset + value.length;
+		return value.length;
+	}
+
+	push(buffer: Buffer) {
+		this.buffer = Buffer.concat([this.buffer, buffer]);
+	}
+
+	seek(offset: number) {
+		this.position += offset;
+		if (this.position < 0) {
+			this.position = 0;
+		} else if (this.position > this.buffer.length - 1) {
+			this.position = this.buffer.length - 1;
+		}
+	}
+
+	setPosition(position: number) {
+		this.position = position;
+		if (this.position < 0) {
+			this.position = 0;
+		} else if (this.position > this.buffer.length - 1) {
+			this.position = this.buffer.length - 1;
+		}
+	}
+
+	getPosition() {
+		return this.position;
+	}
+}
+
+export function createWriter(filePath: string, endian?: 'BE' | 'LE'): Promise<HiFileWriter>;
+export function createWriter(handle: FileHandle, endian?: 'BE' | 'LE'): HiFileWriter;
+export function createWriter(buffer?: Buffer, endian?: 'BE' | 'LE', chunkSize?: number): HiBufferWriter;
+export function createWriter(arg1?: string | FileHandle | Buffer, arg2?: 'BE' | 'LE', arg3?: number) {
+	if (arg1 === undefined || Buffer.isBuffer(arg1)) {
+		return new HiBufferWriter(arg1, arg2, arg3);
+	} else if (typeof arg1 === 'string') {
+		return open(arg1).then(x => new HiFileWriter(x, arg2));
+	} else {
+		return new HiFileWriter(arg1, arg2);
+	}
+}
